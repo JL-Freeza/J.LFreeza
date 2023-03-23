4103e173e2cfbd99d7967f8306421d11879fd96d
JL-Freeza <110564732+JL-Freeza@users.noreply.github.com>
2023-03-22 19:28:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,23 @@
-const { exec } = require('youtube-dl-exec');
+const convertBtn = document.getElementById('convert-btn');
+const audioPlayer = document.getElementById('audio-player');
+const errorMsg = document.getElementById('error-msg');
 
-function convertToMP3() {
-    // Get the YouTube URL from the input field
-    var youtubeUrl = document.getElementById("youtube_url").value;
+convertBtn.addEventListener('click', () => {
+	const youtubeLink = document.getElementById('youtube-link').value.trim();
+	const regex = /^(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/;
 
-    // Check if the URL is valid
-    if (isValidYouTubeUrl(youtubeUrl)) {
-        // Send a request to the server to convert the YouTube video to an MP3 file
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", "convert.php", true);
-        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == XMLHttpRequest.DONE) {
-                // Display the download link for the MP3 file
-                var output = document.getElementById("output");
-                output.innerHTML = "<a href='" + xhr.responseText + "'>Download MP3</a>";
-            }
-        }
-        xhr.send("youtube_url=" + youtubeUrl);
-    } else {
-        // Display an error message if the URL is invalid
-        var output = document.getElementById("output");
-        output.innerHTML = "<p>Invalid YouTube URL. Please enter a valid URL.</p>";
-    }
-
-    return false; // Prevents the form from submitting and reloading the page
-}
-By adding return false at the end of the function, you are telling the browser not to perform the default action of submitting the form and reloading the page.
-
-
-
+	if (regex.test(youtubeLink)) {
+		const videoId = youtubeLink.split('v=')[1];
+		const mp3Url = `https://www.convertmp3.io/fetch/?video=https://www.youtube.com/watch?v=${videoId}`;
+		audioPlayer.src = mp3Url;
+		audioPlayer.style.display = 'block';
+		errorMsg.style.display = 'none';
+	} else {
+		errorMsg.textContent = 'Please enter a valid YouTube link.';
+		errorMsg.style.display = 'block';
+		audioPlayer.style.display = 'none';
+	}
+});
 
 
 
