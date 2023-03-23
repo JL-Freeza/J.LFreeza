2319ee9ce5995f1b58f98628eb36144e910a557a
JL-Freeza <110564732+JL-Freeza@users.noreply.github.com>
2023-03-22 17:14:50 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
+const { exec } = require('youtube-dl-exec');
+
 function convertToMP3() {
-	// Get the YouTube URL from the input field
-	var youtubeUrl = document.getElementById("youtube_url").value;
+  // Get the YouTube URL from the input field
+  const youtubeUrl = document.getElementById('youtube_url').value;
 
-	// Check if the URL is valid
-	if (isValidYouTubeUrl(youtubeUrl)) {
-		// Send a request to the server to convert the YouTube video to an MP3 file
-		var xhr = new XMLHttpRequest();
-		xhr.open("POST", "convert.php", true);
-		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState == XMLHttpRequest.DONE) {
-				// Display the download link for the MP3 file
-				var output = document.getElementById("output");
-				var mp3Url = xhr.responseText;
-if (mp3Url.endsWith(".mp3")) {
-  output.innerHTML = "<a href='" + mp3Url + "'>Download MP3</a>";
-} else {
-  output.innerHTML = "<p>Error converting video to MP3.</p>";
-}
-			}
-		}
-		xhr.send("youtube_url=" + youtubeUrl);
-	} else {
-		// Display an error message if the URL is invalid
-		var output = document.getElementById("output");
-		output.innerHTML = "<p>Invalid YouTube URL. Please enter a valid URL.</p>";
-	}
+  // Check if the URL is valid
+  if (isValidYouTubeUrl(youtubeUrl)) {
+    // Download and convert the YouTube video to an MP3 file
+    const options = {
+      dumpSingleJson: true,
+      noWarnings: true,
+      noCallHome: true,
+      format: 'bestaudio',
+      audioFormat: 'mp3',
+      audioQuality: 0,
+      output: '%(id)s.%(ext)s'
+    };
+
+    exec(youtubeUrl, options)
+      .then(output => {
+        // Display the download link for the MP3 file
+        const downloadUrl = URL.createObjectURL(output[0].data);
+        const outputEl = document.getElementById('output');
+        outputEl.innerHTML = `<a href="${downloadUrl}" download>Download MP3</a>`;
+      })
+      .catch(error => {
+        // Display an error message if the download or conversion failed
+        const outputEl = document.getElementById('output');
+        outputEl.innerHTML = '<p>Error converting video to MP3.</p>';
+        console.error(error);
+      });
+  } else {
+    // Display an error message if the URL is invalid
+    const outputEl = document.getElementById('output');
+    outputEl.innerHTML = '<p>Invalid YouTube URL. Please enter a valid URL.</p>';
+  }
 }
 
-function isValidYouTubeUrl(url) {
-	// Check if the URL matches the pattern for a YouTube video URL
-	var pattern = /^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+/;
-	return pattern.test(url);
-}
