5e3981ec72810c802d005be550f0029c3d248341
JL-Freeza <110564732+JL-Freeza@users.noreply.github.com>
2024-08-21 01:29:03 -0700

    Update music.js

--- music.js
+++ music.js
@@ -4,9 +4,3 @@ function scrollToSection(sectionId) {
         section.scrollIntoView({ behavior: 'smooth' });
     }
 }
-
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Your message has been sent!');
-    // Here you could add code to actually send the form data to your email or backend
-});
